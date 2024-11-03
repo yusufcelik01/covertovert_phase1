d7b5d814d13ed74b6c45366abef8ecf342915d1a
Yusuf Çelik <celikyusuf2@gmail.com>
2024-11-03 20:22:23 +0300

    Implement most basic packet sending mechanism
        Sender sends an ethernet packet and receiver reads the contents by sniffing.
    
        Sender can't find the destination MAC and the packet is broadcasted as result. Should be fixed later

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,21 @@
-import scapy
+import scapy.all as scapy
+import time
 
 # Implement your ICMP receiver here
+def print_packet(packet):
+    packet.show()
+
+#scapy.sniff(prn=print_packet, count=2, iface="lo")
+scapy.sniff(prn=print_packet, count=2, iface="eth0")
+#scapy.sniff(count=2, iface="lo")
+
+"""
+dummyLoad = "EADGBE"
+dummyPkt= scapy.IP() / scapy.ICMP() / dummyLoad
+
+while True:
+    ans, unans = scapy.sr(dummyPkt, verbose=False)
+    for pkt in ans:
+        print(pkt)
+    time.sleep(1)
+    """
--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,5 @@
-import scapy
+import scapy.all as scapy
 
 # Implement your ICMP sender here
+pkt = scapy.Ether(dst="02:42:ac:12:00:03") / scapy.IP(dst="172.18.0.3", ttl=1) / scapy.ICMP() / "DGCFAD"
+scapy.send(pkt)
